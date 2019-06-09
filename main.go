106ed1fd77afbeeeacf867647bd17d9b412aa75a
Happy <mark@venetianplasterfinishes.com.au>
2019-06-09 20:19:52 +1000

    Add struct for request body.

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/mail"
 )
+
+type ReqBody struct {
+	Properties struct {
+		Firstname struct {
+			Value string `json:"value"`
+		} `json:"firstname"`
+		Email struct {
+			Value string `json:"value"`
+		} `json:"email"`
+	} `json:"properties"`
+}
 
 func main() {
 	http.HandleFunc("/", handle)
@@ -30,29 +43,41 @@ func handle(w http.ResponseWriter, r *http.Request) {
 }
 
 func sendMail(w http.ResponseWriter, r *http.Request) {
+	var reqBody ReqBody
+	ctx := appengine.NewContext(r)
 
-	// ctx := appengine.NewContext(r)
-
-	body, err := ioutil.ReadAll(r.Body)
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&reqBody)
 
 	if err != nil {
-		fmt.Println(err)
+		http.Error(w, err.Error(), 500)
+		return
 	}
 
-	fn := gjson.get(body, "properties.firstname")
+	firstName, email := reqBody.Properties.Firstname.Value, reqBody.Properties.Email.Value
 
-	fmt.Fprintf(w, string(fn))
+	if firstName == "" || email == "" {
+		http.Error(w, "BAD REQUEST NO CORRECT DATA", 400)
+		return
+	}
 
-	// if err != nil {
-	// 	http.Error(w, err.Error(), 400)
-	// 	return
-	// }
+	subject := "Terms and Conditions - Acumen Finance"
+	body := fmt.Sprintf("<p> Dear %s,<br>,<p>Thank you for your recent loan submission and engagement via www.acumenfinance.com.au/apply,<br>,<p>This email is to confirm that your application has been received and we will contact you A.S.A.P to progress the transaction further.,<br>,Also please find attached some further information on Acumen Finance and its services and also our standard terms of engagement for your records. ", firstName)
 
-	// ctx := appengine.NewContext(r)
-	// msg := &mail.Message{
-	// 		Sender: "cl@acumenfinance.com.au"
-	// 		To:
-	// }
+	dat, err := ioutil.ReadFile("/tmp/dat")
 
-	// fmt.Fprintf(w, "SENDING MAIL")
+	if err != nil {
+		fmt.Fprintf(w, err.Error(), 500)
+	}
+
+	msg := &mail.Message{
+		Sender:   "cl@acumenfinance.com.au Commercial Loans <cl@acumenfinance.com.au>",
+		To:       []string{email},
+		Subject:  subject,
+		HTMLBody: body,
+	}
+
+	if err := mail.Send(ctx, msg); err != nil {
+		fmt.Fprintf(w, "Coudn't send email: %v", err.Error())
+	}
 }
